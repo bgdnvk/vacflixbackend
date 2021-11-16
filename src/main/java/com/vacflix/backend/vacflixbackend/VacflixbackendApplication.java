c8598ac5c58392d4112be0bb535b5fcd42201293
bgdnvk <bnovykov@gmail.com>
2021-11-17 00:43:44 +0100

    Initial commit

---
+++ src/main/java/com/vacflix/backend/vacflixbackend/VacflixbackendApplication.java
@@ -0,0 +1,13 @@
+package com.vacflix.backend.vacflixbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacflixbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacflixbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vacflix/backend/vacflixbackend/VacflixbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vacflix.backend.vacflixbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacflixbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
