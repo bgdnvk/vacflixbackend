002a86cb92b4b05894f91df85a21ddf0721eeb08
bgdnvk <bnovykov@gmail.com>
2021-11-18 23:22:21 +0100

    test

--- src/main/java/com/vacflix/backend/vacflixbackend/VacflixbackendApplication.java
+++ src/main/java/com/vacflix/backend/vacflixbackend/VacflixbackendApplication.java
@@ -2,12 +2,20 @@ package com.vacflix.backend.vacflixbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class VacflixbackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VacflixbackendApplication.class, args);
+    }
+    @GetMapping("/restcall")
+    public String testcall(@RequestParam(value = "value1", defaultValue = "default value") String randomVar){
+        return String.format("sup %s", randomVar);
     }
 
 }
