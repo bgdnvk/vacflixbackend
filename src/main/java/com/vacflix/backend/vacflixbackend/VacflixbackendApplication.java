60afb3d177ded5047a8a9aa2f72df491d31b87b5
bgdnvk <bnovykov@gmail.com>
2021-12-06 02:36:03 +0100

    oath works

--- src/main/java/com/vacflix/backend/vacflixbackend/VacflixbackendApplication.java
+++ src/main/java/com/vacflix/backend/vacflixbackend/VacflixbackendApplication.java
@@ -1,21 +1,98 @@
 package com.vacflix.backend.vacflixbackend;
 
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.PlaylistListResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
 @SpringBootApplication
 @RestController
 public class VacflixbackendApplication {
+    private static final String CLIENT_SECRETS= "/client_secret.json";
+    private static final Collection<String> SCOPES =
+            Arrays.asList("https://www.googleapis.com/auth/youtube.readonly");
 
-    public static void main(String[] args) {
+    private static final String APPLICATION_NAME = "API code samples";
+    //https://stackoverflow.com/questions/66249053/com-google-api-client-json-jackson2-jacksonfactory-is-deprecated-what-are-my
+//    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+
+
+    /**
+     * Create an authorized Credential object.
+     *
+     * @return an authorized Credential object.
+     * @throws IOException
+     */
+    public static Credential authorize(final NetHttpTransport httpTransport) throws IOException {
+        // Load client secrets.
+        InputStream in = ApiExample.class.getResourceAsStream(CLIENT_SECRETS);
+        //https://stackoverflow.com/questions/16570523/getresourceasstream-returns-null
+//        assert in != null;
+//        System.out.println(in);
+        GoogleClientSecrets clientSecrets =
+                GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow =
+                new GoogleAuthorizationCodeFlow.Builder(httpTransport, JSON_FACTORY, clientSecrets, SCOPES)
+                        .build();
+        Credential credential =
+                new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
+        return credential;
+    }
+
+    /**
+     * Build and return an authorized API client service.
+     *
+     * @return an authorized API client service
+     * @throws GeneralSecurityException, IOException
+     */
+    public static YouTube getService() throws GeneralSecurityException, IOException {
+        final NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        Credential credential = authorize(httpTransport);
+        return new YouTube.Builder(httpTransport, JSON_FACTORY, credential)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
+
+
+    public static void main(String[] args)  {
         SpringApplication.run(VacflixbackendApplication.class, args);
     }
     @GetMapping("/restcall")
-    public String testcall(@RequestParam(value = "value1", defaultValue = "World") String randomVar){
-        return String.format("Hello %s", randomVar);
+    public String testcall(@RequestParam(value = "value1", defaultValue = "World") String randomVar) throws GeneralSecurityException, IOException {
+//        return String.format("Hello %s", randomVar);
+
+        YouTube youtubeService = getService();
+        // Define and execute the API request
+        YouTube.Playlists.List request = youtubeService.playlists()
+                .list(Collections.singletonList("snippet,contentDetails"));
+        PlaylistListResponse response = request.setChannelId("UC_x5XG1OV2P6uZZ5FSM9Ttw")
+                .setMaxResults(25L)
+                .execute();
+        System.out.println(response);
+        return String.format(String.valueOf(response));
     }
 
 }
