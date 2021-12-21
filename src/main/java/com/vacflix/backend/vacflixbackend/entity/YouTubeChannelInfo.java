90b932196ab18117e0d3a23cb5515cdaddc99ffa
bgdnvk <bnovykov@gmail.com>
2021-12-21 23:43:14 +0100

    db entities added

---
+++ src/main/java/com/vacflix/backend/vacflixbackend/entity/YouTubeChannelInfo.java
@@ -0,0 +1,19 @@
+package com.vacflix.backend.vacflixbackend.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity(name = "youtube_channel")
+@Data
+@EqualsAndHashCode(callSuper=true)
+public class YouTubeChannelInfo extends BaseEntity{
+    @Column(name = "channel_id")
+    private String channelId;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "subscription_count")
+    private long subscriptionCount;
+}
