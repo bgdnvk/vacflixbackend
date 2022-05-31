31d880d2c5e7a861e065663f75202b42a6e59475
bgdnvk <bnovykov@gmail.com>
2022-05-31 17:35:48 +0200

    new controllers added

---
+++ src/main/java/com/vacflix/backend/vacflixbackend/api/CrawlController.java
@@ -0,0 +1,8 @@
+package com.vacflix.backend.vacflixbackend.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+//TODO: put all crawl commands here
+@RestController
+public class CrawlController {
+}
---
+++ src/main/java/com/vacflix/backend/vacflixbackend/api/SearchController.java
@@ -0,0 +1,7 @@
+package com.vacflix.backend.vacflixbackend.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SearchController {
+}
