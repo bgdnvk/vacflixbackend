c1c3b2a0405b648caa5a11eaa56b71875aae5abb
bgdnvk <bnovykov@gmail.com>
2022-06-01 22:30:13 +0200

    b4 searchService impl

---
+++ src/main/java/com/vacflix/backend/vacflixbackend/services/search/SearchService.java
@@ -0,0 +1,35 @@
+package com.vacflix.backend.vacflixbackend.services.search;
+
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.SearchListResponse;
+import com.vacflix.backend.vacflixbackend.auth.ApiAuth;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.Collections;
+
+//service that acts as a proxy for searching queries
+@Service
+public class SearchService {
+
+    @Autowired
+    private Environment env;
+
+    public String getVideosFromQuery(String query) throws IOException {
+        ApiAuth auth = new ApiAuth(env);
+        YouTube youTube = auth.getYoutube();
+
+        // Define and execute the API request
+        YouTube.Search.List request = youTube.search()
+                .list(Collections.singletonList("snippet"));
+        SearchListResponse response = request.setMaxResults(25L)
+                .setQ(query)
+                .execute();
+        //console log
+        System.out.println(response.toString());
+        //TODO: send back a JSON?
+        return response.toString();
+    }
+}
